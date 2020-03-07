4f0cc9a0f023cd2d0fb700e4262b0d9af117fdbd
Sergey Khlystov <itmrgodsend@gmail.com>
2020-03-07 20:16:48 +0300

    Finish Credit

--- main.js
+++ main.js
@@ -4,11 +4,27 @@ const term = document.getElementById('term').value;
 const myButton = document.getElementById('button');
 
 
-
+    
     const percentMonth = percent / 100 / 12;
     const monthPay = sum * (percentMonth + (percentMonth / ((1 + percentMonth) ** 12 - 1)));
+    const totalSum = monthPay * term;
     //console.log(monthPay);
 
   myButton.addEventListener('click', function() {
-    console.log(monthPay);
+    
+    const newItem1 = document.createElement('li');
+    const newItem2 = document.createElement('li');
+    const newItem3 = document.createElement('li');
+    const newItem4 = document.createElement('li');
+    //style
+    
+    newItem1.textContent = "Ежемесячный платёж:    "  + monthPay.toFixed( 0 ) + " рублей";
+    newItem2.textContent = "Ежемесячный процент:    " + percentMonth.toFixed( 5 ) + "%";
+    newItem3.textContent = "Общая сумма кредита:    " + totalSum.toFixed( 0 ) + " рублей";
+    newItem4.textContent = "Переплата по кредиту:    " + (totalSum - sum).toFixed( 0 ) + " рублей";
+   
+    list.appendChild(newItem1);
+    list.appendChild(newItem2);
+    list.appendChild(newItem3);
+    list.appendChild(newItem4);
  });
